c2399548ca36ee718c97c7b279b18cb455754176
Júlia <juliammneves@gmail.com>
2025-03-10 20:51:37 -0300

    Att 3 (Aula 3)

--- src/main/java/com/github/juliammneves/study_api/ControllerProduto.java
+++ src/main/java/com/github/juliammneves/study_api/ControllerProduto.java
@@ -5,34 +5,30 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduto {
-
-    public ResponseEntity<String> demo(){
-        return ResponseEntity.status(203).body("Mensagem teste");
-    }
-
     @PostMapping
-    public ResponseEntity<String> create(){
+    public ResponseEntity<String> create(@RequestBody Produto produto) {
         return ResponseEntity.status(201).body("Produto cadastrado: maçã");
     }
 
     @PutMapping
-    public ResponseEntity<String> update(){
+    public ResponseEntity<String> update() {
         return ResponseEntity.status(201).body("Produto atualizado: maçã");
     }
 
     @GetMapping
-    public ResponseEntity<String> find(){
-        return ResponseEntity.status(201).body("maçã");
+    public ResponseEntity<String> find() {
+        return ResponseEntity.status(201).body("Maçã");
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> delete(){
+    public ResponseEntity<Void> delete() {
         return ResponseEntity.status(204).build();
     }
 }
---
+++ src/main/java/com/github/juliammneves/study_api/Produto.java
@@ -0,0 +1,19 @@
+package com.github.juliammneves.study_api;
+
+public class Produto {
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
